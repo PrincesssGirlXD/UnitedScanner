9599a1751070f11253036852a47ad972e29b5ce2
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2022-12-10 10:09:30 +0530

    Update __main__.py

--- UnitedScanner/__main__.py
+++ UnitedScanner/__main__.py
@@ -9,19 +9,19 @@ from Scanner.vars import API_ID, API_HASH, BOT_TOKEN, LOG_CHANNEL_ID
 
 async def load_start():
     count = num_gbanned_users()
-    LOGGER.info(f"Current Gbanned Users: {count}")
+    LOGGER.info(f"United System Started")
     LOGGER.info("[INFO]: STARTED")
     LOGGER.info(f"LOG CHANNELS: {int(LOG_CHANNEL_ID)}")
     try:
         await pbot.send_message(
-            int(LOG_CHANNEL_ID), f"**Pyrogram Client Started Successfully !!**\nCurrent Gbanned Users: `{count}`"
+            int(LOG_CHANNEL_ID), f"**United System Started"
         )
         LOGGER.info("[INFO]: PYROGRAM BOT STARTED")
     except Exception as e:
         LOGGER.info(f"Bot wasn't able to semd message in your log channel.\n\nERROR: {e}")
     try:
         await ubot.send_message(
-            int(LOG_CHANNEL_ID), "**Assistant Started Successfully !!**"
+            int(LOG_CHANNEL_ID), "**MongoDB Connected Successfully**"
         )
         LOGGER.info("[INFO]: PYROGRAM UserBOT STARTED")
     except Exception as e:
@@ -32,7 +32,7 @@ loop = asyncio.get_event_loop_policy().get_event_loop()
 loop.run_until_complete(load_start())
 
 Client(
-    name="SOME-1HING",
+    name="United",
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
@@ -41,7 +41,7 @@ Client(
     workdir=DOWNLOAD_DIRECTORY,
     sleep_threshold=60,
     in_memory=True,
-    plugins={"root": "Scanner.plugins"},
+    plugins={"root": "UnitedScanner.plugins"},
 ).start()
 
 loop.close()
