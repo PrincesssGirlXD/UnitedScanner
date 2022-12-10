6e425b1f4030d9984c52fe3c2ab6448845518bfe
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2022-12-10 12:46:41 +0530

    Update __main__.py

--- UnitedScanner/__main__.py
+++ UnitedScanner/__main__.py
@@ -3,25 +3,25 @@ import requests
 
 from pyrogram import Client
 
-from Scanner import LOGGER, pbot, ubot, tbot
-from Scanner.db.global_bans_db import num_gbanned_users
-from Scanner.vars import API_ID, API_HASH, BOT_TOKEN, LOG_CHANNEL_ID
+from UnitedScanner import LOGGER, pbot, ubot
+#from Scanner.db.global_bans_db import num_gbanned_users
+from UnitedScanner import API_ID, API_HASH, BOT_TOKEN, LOG_CHANNEL_ID
 
 async def load_start():
-    count = num_gbanned_users()
-    LOGGER.info(f"Current Gbanned Users: {count}")
+    #count = num_gbanned_users()
+    LOGGER.info(f"United System Started")
     LOGGER.info("[INFO]: STARTED")
     LOGGER.info(f"LOG CHANNELS: {int(LOG_CHANNEL_ID)}")
     try:
         await pbot.send_message(
-            int(LOG_CHANNEL_ID), f"**Pyrogram Client Started Successfully !!**\nCurrent Gbanned Users: `{count}`"
+            int(LOG_CHANNEL_ID), "United System Started"
         )
         LOGGER.info("[INFO]: PYROGRAM BOT STARTED")
     except Exception as e:
         LOGGER.info(f"Bot wasn't able to semd message in your log channel.\n\nERROR: {e}")
     try:
         await ubot.send_message(
-            int(LOG_CHANNEL_ID), "**Assistant Started Successfully !!**"
+            int(LOG_CHANNEL_ID), "**MongoDB Connected Successfully**"
         )
         LOGGER.info("[INFO]: PYROGRAM UserBOT STARTED")
     except Exception as e:
@@ -32,16 +32,12 @@ loop = asyncio.get_event_loop_policy().get_event_loop()
 loop.run_until_complete(load_start())
 
 Client(
-    name="SOME-1HING",
+    name="UnitedScanner",
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
-    workers=min(32, os.cpu_count() + 4),
-    parse_mode=ParseMode.DEFAULT,
-    workdir=DOWNLOAD_DIRECTORY,
-    sleep_threshold=60,
     in_memory=True,
-    plugins={"root": "Scanner.plugins"},
+    plugins={"root": "UnitedScanner.plugins"},
 ).start()
 
 loop.close()
