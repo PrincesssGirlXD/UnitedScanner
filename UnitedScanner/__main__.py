c30aeafc76c5a6963534b5fbfb40e3463b473302
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2022-12-10 12:35:58 +0530

    Update __main__.py

--- UnitedScanner/__main__.py
+++ UnitedScanner/__main__.py
@@ -44,10 +44,10 @@ async def load_start():
     except Exception as e:
         LOGGER.info(f"UserBot wasn't able to semd message in your log channel.\n\nERROR: {e}")
     
-loop = asyncio.new_event_loop()
+#loop = asyncio.new_event_loop()
 #asyncio.set_event_loop(loop)
-loop.run_until_complete(load_start())
-loop.close()
+#loop.run_until_complete(load_start())
+#loop.close()
 
 Client(
     name="United",
