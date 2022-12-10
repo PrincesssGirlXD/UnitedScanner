536eefeb95bc7fd0d8bb1dcf66c2d51c721b2142
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2022-12-10 10:19:06 +0530

    Update __init__.py

--- UnitedScanner/db/__init__.py
+++ UnitedScanner/db/__init__.py
@@ -5,15 +5,15 @@ from motor import motor_asyncio
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
 
-from Scanner import LOGGER
-from Scanner.vars import MONGO_DB_URI 
+from UnitedScanner import LOGGER
+from UnitedScanner.config import MONGO_DB_URI 
 
 
 client = MongoClient()
-client = MongoClient(MONGO_DB_URI, 27017)["Scanner"]
+client = MongoClient(MONGO_DB_URI, 27017)["UnitedScanner"]
 motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, 27017)
-db = motor["Scanner"]
-db = client["Scanner"]
+db = motor["UnitedScanner"]
+db = client["UnitedScanner"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
