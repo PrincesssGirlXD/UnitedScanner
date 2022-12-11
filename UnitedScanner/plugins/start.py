2eeb280a21d495bebfe40d691962779bc7f97ed4
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2022-12-11 13:43:57 +0530

    Update start.py

--- UnitedScanner/plugins/start.py
+++ UnitedScanner/plugins/start.py
@@ -20,7 +20,7 @@ TIME_DURATION_UNITS = (
 )
 
 
-CAPTION = f"""Hᴇʟʟᴏ {mention()}
+CAPTION = f"""Hᴇʟʟᴏ message.from_user.mention()
 I Wɪʟʟ Hᴇʟᴘ Yᴏᴜ Tᴏ Pʀᴏᴛᴇᴄᴛ Yᴏᴜ Fʀᴏᴍ Pᴏᴛᴇɴᴛɪᴀʟ Tʜʀᴇᴀᴛ."""
 
 @Client.on_message(command("start") & filters.private)
