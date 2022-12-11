07f06c016b4acd80206c01149a48e967ef0b10f2
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2022-12-11 16:06:47 +0530

    Update start.py

--- UnitedScanner/plugins/start.py
+++ UnitedScanner/plugins/start.py
@@ -20,7 +20,7 @@ TIME_DURATION_UNITS = (
 )
 
 
-CAPTION = "Hᴇʟʟᴏ message.from_user.mention()\nI Wɪʟʟ Hᴇʟᴘ Yᴏᴜ Tᴏ Pʀᴏᴛᴇᴄᴛ Yᴏᴜ Fʀᴏᴍ Pᴏᴛᴇɴᴛɪᴀʟ Tʜʀᴇᴀᴛ."
+CAPTION = f"""**Hᴇʟʟᴏ message.from_user.mention()\nI Wɪʟʟ Hᴇʟᴘ Yᴏᴜ Tᴏ Pʀᴏᴛᴇᴄᴛ Yᴏᴜ Fʀᴏᴍ Pᴏᴛᴇɴᴛɪᴀʟ Tʜʀᴇᴀᴛ.**"""
 
 @Client.on_message(command("start") & filters.private)
 async def start_(client: Client, message: Message):
@@ -28,7 +28,7 @@ async def start_(client: Client, message: Message):
     accha = await message.reply("United Scanner Starting ✨")
     await accha.delete()
     await asyncio.sleep(0.1)
-    await message.reply_photo("https://telegra.ph/file/e3c8a57b3ebcd0b1c8529.mp4" , "op" ,
+    await message.reply_video("https://telegra.ph/file/e3c8a57b3ebcd0b1c8529.mp4" , CAPTION ,
     reply_markup=InlineKeyboardMarkup(
             [
                 [
@@ -47,7 +47,7 @@ async def start_grp(client: Client, message: Message):
     accha = await message.reply("United Scanner Starting ✨")
     await accha.delete()
     await asyncio.sleep(0.1)
-    await message.reply_photo("https://telegra.ph/file/e3c8a57b3ebcd0b1c8529.mp4" , CAPTION ,
+    await message.reply_video("https://telegra.ph/file/e3c8a57b3ebcd0b1c8529.mp4" , CAPTION ,
     reply_markup=InlineKeyboardMarkup(
             [
                 [
