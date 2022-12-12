606654fd53029614ffebe92a7d34fdc041159100
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2022-12-12 14:26:49 +0530

    Update info.py

--- UnitedScanner/plugins/info.py
+++ UnitedScanner/plugins/info.py
@@ -2,8 +2,8 @@ import os
 
 from pyrogram import filters
 from pyrogram import Client as bot
-from pyrogram.types import Message 
-from pyrogram.types import Chat as chat
+from pyrogram.types import Message as chat
+from pyrogram.types import Chat 
 from UnitedScanner.config import INSPECTORS
 from UnitedScanner.utils import sections
 
